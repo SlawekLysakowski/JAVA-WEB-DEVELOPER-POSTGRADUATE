24786ca93c3f68ad7a2755d6594e8964ddc5ba0e
SlawekLysakowski <H4slo11!>
2021-12-05 20:50:45 +0100

    Pierwszy commit

--- src/Podyplomowe.java
+++ src/Podyplomowe.java
@@ -2,6 +2,7 @@ public class Podyplomowe {
     public static void main(String[] args) {
         System.out.println("Ten program nie robi nic ciekawego");
         System.out.println("ale moze kiedys to sie zmieni :)");
+        // dodano dowolny komentarz
 
     }
 }
